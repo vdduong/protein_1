d607429f046039133a5774edf634fb65cd62301c
vdduong <vdduong@free.fr>
2014-01-06 03:39:29 +0100

    Update protein_10.py

--- protein_10.py
+++ protein_10.py
@@ -1,2 +1,19 @@
 # increase 10% of all measurable distances
 # compare created and original distance matrix to conclude.
+
+def increase_10(d):
+  for key_ in d.keys();
+    if d[key_] < 5.5:
+      d[key_] = d[key_]*1.1
+    else:
+      pass
+  d = triangle_inequality(d)
+  return d
+  
+def comparing_distance_matrix(d_new,d):
+  sum = 0.0
+  for key_ in d.keys():
+    sum += d_new[key_]**2.0 - d[key_]**2.0
+  sum = sum/2.0
+  return sum
+  
