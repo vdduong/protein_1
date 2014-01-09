74ad072cf0a2c39f06d05e6076e0acc0db8eb37d
vdduong <vdduong@free.fr>
2014-01-09 17:50:49 +0100

    Update protein_10.py

--- protein_10.py
+++ protein_10.py
@@ -1,5 +1,6 @@
 # increase 10% of all measurable distances
 # compare created and original distance matrix to conclude.
+import numpy as np
 
 def increase_10(d):
   for key_ in d.keys();
@@ -18,4 +19,30 @@ def comparing_distance_matrix(d_new,d):
   return sum
   
 def update_distance_matrix():
-  pass
+  """ updating distance matrix from clusters of configurations and their respective difference against initial \
+      distance matrix """
+  global vertices
+  # score based on distance matrix difference ???
+  score = math.sqrt(comparing_distance_matrix(d_new, d))/len(vertices)
+  
+  # non linear scaled inter-atomic distance variance matrix
+  sigma = np.zeros((len(vertices), len(vertices)))
+  count_sigma = 0
+  for configuration in dict_configuration:
+    sigma[i][j]+= (d_new[i][j] - d[i][j])**2
+    count_sigma+=1
+  sigma = sigma/count_sigma
+  
+  # creating the matrix D
+  D = np.zeros((len(vertices), len(vertices)))
+  D[i][j] = norm((h[i] - h[j])) # norm to be defined
+  # scaling non linear 
+  h[i][j] = math.log(1 + sigma[i][j]**2/k_off)
+  
+  Z[diagonal] = 1 - 1/len(vertices)
+  Z[non_diagonal] = - 1/len(vertices)
+  
+  G = -0.5*Z*D*Z.T
+  
+  
+  break
