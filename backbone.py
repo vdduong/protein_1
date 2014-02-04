250f914a6b2080b664d3d0ea794973b1e126b44f
vdduong <vdduong@free.fr>
2014-02-04 16:43:25 +0100

    Update backbone.py

--- backbone.py
+++ backbone.py
@@ -1,18 +1,12 @@
-# backbone
-
-sd_helix = 4.3 # ??
-sd_sheet = 2.7 # ??
-sd_loop = 3.5 # ??
-tol_distance = 0.05 # ??
-
-def prob_helix(name_distance):
-  return math.exp(-0.5*(dict_distance[name_distance]-sd_helix)**2/tol_distance**2)
-
-def prob_sheet(name_distance):
-  return math.exp(-0.5*(dict_distance[name_distance]-sd_sheet)**2/tol_distance**2)
-
-def prob_loop(name_distance):
-  return math.exp(-0.5*(dict_distance[name_distance]-sd_loop)**2/tol_distance**2)
-
-# given the distance matrix between HN (correct or not), return the most probable sequential chain of HN
-
+class assignment:
+	def __init__(element_1, element_2, relation):
+		self.element_1 = element_1
+		self.element_2 = element_2
+		self.relation = relation
+	
+def build_fragment(assigment_1, assignment_2):
+	if assignment_1.element_1 = assignment_2.element_2:
+		relation_between_fragment = relation(assigment_1.relation, assignment_2.relation)
+		return general_fragment
+	else:
+		return None
